91400ce7d1bf554f91e5ce991e327cb246952c65
Enrique Pando <enr.pando@gmail.com>
2021-09-06 16:14:11 -0500

    Elegir titulo en header segun la URL

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div class="h-14">
     <nav class="fixed w-full h-14 p-2 flex justify-between items-center bg-green-600 shadow-md">
       <div class="w-10 h-10 hidden sm:block bg-white opacity-30"></div>
-      <span class="mx-1 text-white font-medium sm:text-center sm:text-lg lg:text-xl">Área de Investigación Foránea</span>
+      <span class="mx-1 text-white font-medium sm:text-center sm:text-lg lg:text-xl">{{ title }}</span>
       <button class="w-10 h-10 flex justify-center items-center rounded-sm hover:bg-transparent-white active:bg-transparent-black transition duration-150" @click="$emit('openSidenav')">
         <i class="fas fa-bars text-white text-xl"></i>
       </button>
@@ -11,8 +11,32 @@
 </template>
 
 <script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import contenido from '@/contenido.json'
+
 export default {
-  emits: ['openSidenav']
+  emits: ['openSidenav'],
+  setup() {
+    const route = useRoute();
+    
+    const title = computed(() => {
+      let found = null;
+
+      contenido.sidenav.some(i => {
+        const result = i.list.find(j => +j.sec === +route.query.sec);
+
+        if (result !== undefined) {
+          found = result;
+          return true;
+        }
+      })
+
+      return found.name;
+    });
+
+    return { title };
+  }
 }
 </script>
 
